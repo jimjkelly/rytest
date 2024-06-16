5de5a38cb2d5fac0ae187a0ab985d4cdebb1b70a
James Kelly <pthread1981@gmail.com>
2024-06-15 18:04:18 -0600

    Initial ability to run tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use std::error::Error;
+use std::path::Path;
 use std::sync::mpsc::{self, RecvError};
-use std::thread;
+use std::{env, fs, thread};
 use clap::{App, Arg};
 use std::fs::File;
 use std::io::Read;
 use rustpython_parser::{Parse, ast}; 
 use rustpython_parser::ast::Stmt::FunctionDef;
 use glob::glob;
+
+use pyo3::prelude::*;
+use pyo3::types::PyList;
 
 type Rysult<T> = Result<T, Box<dyn Error>>;
 
@@ -26,7 +30,7 @@ pub fn get_args() -> Rysult<Config> {
     let matches = App::new("rytest")
         .version("0.1.0")
         .author("Jim Kelly <pthread1981@gmail.com>")
-        .about("rytest is a reasonably fast Python test runner.")
+        .about("rytest is a reasonably fast, somewhat Pytest compatible Python test runner.")
         .arg(
             Arg::with_name("files")
                 .value_name("FILE")
@@ -93,7 +97,6 @@ pub fn find_files(paths: Vec<String>, watch: bool, tx: mpsc::Sender<String>) -> 
             match entry {
                 Ok(p) => {
                     if p.is_file() && p.file_stem().unwrap().to_string_lossy().starts_with("test_") && p.extension().unwrap() == "py" {
-                        println!("Would send file: {}", p.to_str().unwrap());
                         tx.send(p.to_str().unwrap().to_string())?;
                     }
                 },
@@ -118,10 +121,8 @@ pub fn find_tests(prefix: String, rx: mpsc::Receiver<String>, tx: mpsc::Sender<T
                 let mut data = String::new();
                 let mut file = File::open(file_name.clone())?;
                 file.read_to_string(&mut data)?;
-                println!("Would read file: {}", file_name);
                 let ast = ast::Suite::parse(data.as_str(), "<embedded>");
 
-    
                 match ast {
                     Ok(ast) => {
                         for stmt in ast {
@@ -138,7 +139,7 @@ pub fn find_tests(prefix: String, rx: mpsc::Receiver<String>, tx: mpsc::Sender<T
                         }
                     
                     },
-                    Err(e) => println!("Error parsing file: {}", e),
+                    Err(e) => println!("Error parsing {}: {}", file_name, e),
                 }
             },
             // TODO: Handle this better - should we be able to tell the difference between the channel being closed and an error?
@@ -153,11 +154,32 @@ pub fn run_tests(rx: mpsc::Receiver<TestCase>, tx: mpsc::Sender<TestCase>) -> Ry
     loop {
         match rx.recv() {
             Ok(mut test) => {
-                // We should, ya know, actually run the test.
-                test.passed = true;
-                println!("Would run test: {}:{}", test.file, test.test);
+                let current_dir = env::current_dir().unwrap();
+                let path = Path::new(&current_dir);
+                let py_code = fs::read_to_string(path.join(test.file.clone()))?;
+
+                let result = Python::with_gil(|py| -> PyResult<Py<PyAny>>{
+                    let syspath = py.import_bound("sys").unwrap().getattr("path").unwrap().downcast_into::<PyList>().unwrap();
+                    syspath.insert(0, &path).unwrap();
+
+                    let module = PyModule::from_code_bound(py, &py_code, "", "");
+                    if module.is_err() {
+                        return Err(module.unwrap_err())
+                    }
+                    
+                    let module = module.unwrap();
+                    let app = module.getattr(test.test.as_str());
+                    if app.is_err() {
+                        return Err(app.unwrap_err())
+                    }
+                    
+                    let app: Py<PyAny> = app.unwrap().into();
+                    app.call0(py)
+                });
+
+                test.passed = result.is_ok();
                 tx.send(test)?;
-            },
+            }, 
             // TODO: Handle this better - should we be able to tell the difference between the channel being closed and an error?
             Err(RecvError) => break,
         }
@@ -167,15 +189,25 @@ pub fn run_tests(rx: mpsc::Receiver<TestCase>, tx: mpsc::Sender<TestCase>) -> Ry
 }
 
 pub fn output_results(rx: mpsc::Receiver<TestCase>) -> Rysult<()> {
+    let mut passed = 0;
+    let mut failed = 0;
+
     loop {
         match rx.recv() {
             Ok(result) => {
-                println!("{}:{} - {}", result.file, result.test, result.passed);
+                println!("{}:{} - {}", result.file, result.test, if result.passed { "PASSED" } else { "FAILED" });
+                if result.passed {
+                    passed += 1;
+                } else {
+                    failed += 1;
+                }
             },
             // TODO: Handle this better - should we be able to tell the difference between the channel being closed and an error?
             Err(RecvError) => break,
         }
     }
+
+    println!("{} passed, {} failed", passed, failed);
 
     Ok(())
 }
