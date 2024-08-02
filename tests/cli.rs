f253770cedc6b4abf2463b120d3e7bcd86882ca0
James Kelly <pthread1981@gmail.com>
2024-08-01 21:52:39 -0600

    Collect Only Flag
    This PR adds support for the `--collect-only` flag, which much like pytest
    will run through and simply do test collection.  It also adds support for
    timing the run as pytest does as well as adding e2e testing of the new
    command.
    
    It also removes a little unused code around watch mode and fixture collection
    as well as renaming the `test` field on the `TestCase` struct to `name`
    so it reads as `test.name` rather than `test.test`.
    
    Closes #9

---
+++ tests/cli.rs
@@ -0,0 +1,42 @@
+use assert_cmd::Command;
+use predicates::prelude::*;
+use std::fs;
+
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
+const PRG: &str = "rytest";
+
+fn run(args: &[&str], expected_file: &str) -> TestResult {
+    println!("expected {}", &expected_file);
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin(PRG)?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
+}
+
+#[test]
+fn collect_errors() -> TestResult {
+    run(
+        &["tests/**/*.py", "--collect-only"],
+        "tests/expected/collect_two_errors.out",
+    )
+}
+
+#[test]
+fn collect_error() -> TestResult {
+    run(
+        &["tests/input/bad/*.py", "--collect-only"],
+        "tests/expected/collect_one_error.out",
+    )
+}
+
+#[test]
+fn collect() -> TestResult {
+    run(
+        &["tests/input/good/*.py", "--collect-only"],
+        "tests/expected/collect.out",
+    )
+}
