be5c5fe0cf0548cd94975d3eb2aa946055c362d8
James Kelly <pthread1981@gmail.com>
2024-08-25 12:03:30 -0600

    Filter output to remove changing time

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,9 +5,19 @@ fn cli() -> Command {
     Command::new(get_cargo_bin("rytest"))
 }
 
+fn setup() -> insta::Settings {
+    let mut settings = insta::Settings::clone_current();
+    settings.add_filter(r"in [[:xdigit:]]+\.[[:xdigit:]]{2}s", "in <TIME>s");
+
+    settings
+}
+
+
 #[test]
 fn help() {
-    assert_cmd_snapshot!(cli().arg("--help"), @r###"
+    let settings = setup();
+
+    settings.bind(|| assert_cmd_snapshot!(cli().arg("--help"), @r###"
     success: true
     exit_code: 0
     ----- stdout -----
@@ -31,60 +41,66 @@ fn help() {
         <FILE>...    Input file(s) [default: .]
 
     ----- stderr -----
-    "###);
+    "###));
 }
 
 #[test]
 fn collect_errors() {
-    assert_cmd_snapshot!(cli().arg("tests").arg("--collect-only"), @r###"
-    success: true
-    exit_code: 0
-    ----- stdout -----
-    tests/input/classes/test_classes.py::SomeTest::test_something
-    tests/input/classes/test_classes.py::SomeTest::test_something_else
-    tests/input/classes/test_classes.py::SomeTest::test_assert_failure
-    ERROR tests/input/bad/test_other_error.py
-    tests/input/bad/test_other_file.py::test_function_passes
-    tests/input/bad/test_other_file.py::test_function_fails
-    tests/input/folder/test_another_file.py::test_another_function
-    tests/input/folder/test_another_file.py::test_function_with_decorator
-    ERROR tests/input/test_bad_file.py
-    tests/input/good/test_success.py::test_success
-    tests/input/good/test_success.py::test_more_success
-    tests/input/test_file.py::test_function_passes
-    tests/input/test_file.py::test_function_fails
-    tests/input/test_fixtures.py::test_fixture
-    12 tests collected, 2 errors in 0.00s
+    let settings = setup();
 
-    ----- stderr -----
-    "###);
+    settings.bind(|| assert_cmd_snapshot!(cli().arg("tests").arg("--collect-only"), @r###"
+                     success: true
+                     exit_code: 0
+                     ----- stdout -----
+                     tests/input/classes/test_classes.py::SomeTest::test_something
+                     tests/input/classes/test_classes.py::SomeTest::test_something_else
+                     tests/input/classes/test_classes.py::SomeTest::test_assert_failure
+                     ERROR tests/input/bad/test_other_error.py
+                     tests/input/bad/test_other_file.py::test_function_passes
+                     tests/input/bad/test_other_file.py::test_function_fails
+                     tests/input/folder/test_another_file.py::test_another_function
+                     tests/input/folder/test_another_file.py::test_function_with_decorator
+                     ERROR tests/input/test_bad_file.py
+                     tests/input/good/test_success.py::test_success
+                     tests/input/good/test_success.py::test_more_success
+                     tests/input/test_file.py::test_function_passes
+                     tests/input/test_file.py::test_function_fails
+                     tests/input/test_fixtures.py::test_fixture
+                     12 tests collected, 2 errors in <TIME>s
+
+                     ----- stderr -----
+                     "###));
 }
 
 #[test]
 fn collect_error() {
-    assert_cmd_snapshot!(cli().arg("tests/input/bad").arg("--collect-only"), @r###"
-    success: true
-    exit_code: 0
-    ----- stdout -----
-    ERROR tests/input/bad/test_other_error.py
-    tests/input/bad/test_other_file.py::test_function_passes
-    tests/input/bad/test_other_file.py::test_function_fails
-    2 tests collected, 1 error in 0.00s
+    let settings = setup();
 
-    ----- stderr -----
-    "###);
+    settings.bind(|| assert_cmd_snapshot!(cli().arg("tests/input/bad").arg("--collect-only"), @r###"
+                     success: true
+                     exit_code: 0
+                     ----- stdout -----
+                     ERROR tests/input/bad/test_other_error.py
+                     tests/input/bad/test_other_file.py::test_function_passes
+                     tests/input/bad/test_other_file.py::test_function_fails
+                     2 tests collected, 1 error in <TIME>s
+
+                     ----- stderr -----
+                     "###));
 }
 
 #[test]
 fn collect() {
-    assert_cmd_snapshot!(cli().arg("tests/input/good").arg("--collect-only"), @r###"
-    success: true
-    exit_code: 0
-    ----- stdout -----
-    tests/input/good/test_success.py::test_success
-    tests/input/good/test_success.py::test_more_success
-    2 tests collected in 0.00s
+    let settings = setup();
 
-    ----- stderr -----
-    "###);
+    settings.bind(|| assert_cmd_snapshot!(cli().arg("tests/input/good").arg("--collect-only"), @r###"
+                     success: true
+                     exit_code: 0
+                     ----- stdout -----
+                     tests/input/good/test_success.py::test_success
+                     tests/input/good/test_success.py::test_more_success
+                     2 tests collected in <TIME>s
+
+                     ----- stderr -----
+                     "###));
 }
